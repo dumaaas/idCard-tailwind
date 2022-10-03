582e200867085acc1a1f0a2617a23404f45b2e40
dumaaas <markodumnic8@gmail.com>
2022-10-04 00:55:02 +0200

    ID Card

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        rubik: ['Rubik', 'sans-serif'],
+        cedarville: ['Cedarville Cursive', 'sans-serif'],
+        segoe: ['Segoe UI', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
